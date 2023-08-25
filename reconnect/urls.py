ca352619f982c5631d2259f57bb9d843d02cda80
Matthew Hedges <matthedges35@gmail.com>
2023-08-24 19:39:05 -0500

    urls page done

--- reconnect/urls.py
+++ reconnect/urls.py
@@ -1,22 +1,24 @@
-"""
-URL configuration for reconnect project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
+from django.conf.urls import include
 from django.urls import path
+from reconnectapi.views import register_user, login_user
+from django.conf.urls import include
+from rest_framework import routers
+from reconnectapi.views import PostView, CommentView, FriendshipView, GroupView, MembershipView, TopicView
+
+
+from django.contrib import admin
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'posts', PostView, 'post')
+router.register(r'comments', CommentView, 'comment')
+router.register(r'friendships', FriendshipView, 'friendship')
+router.register(r'groups', GroupView, 'group')
+router.register(r'memberships', MembershipView, 'membership')
+router.register(r'topics', TopicView, 'topic')
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
+    path('register', register_user),
+    path('login', login_user),
+    path('admin/', admin.site.urls),
+    path('', include(router.urls))
 ]
---
+++ reconnectapi/views/__init__.py
@@ -0,0 +1,7 @@
+from .post_view import PostView
+from .comment_view import CommentView
+from .friendship_view import FriendshipView
+from .group_view import GroupView
+from .membership_view import MembershipView
+from .topic_view import TopicView
+from .auth import register_user, login_user
